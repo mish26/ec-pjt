e1f5a2b314620092f06fb8db309309211398004d
mishrun <mishrun.git@gmail.com>
2018-05-13 23:10:46 +0900

    add pjt

---
+++ src/main/java/com/pjtec/PjtEcApplication.java
@@ -0,0 +1,12 @@
+package com.pjtec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjtEcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjtEcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pjtec/PjtEcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pjtec;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PjtEcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
